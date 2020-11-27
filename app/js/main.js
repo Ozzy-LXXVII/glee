3fa999a1bf177423a975e9372603a9f33b450090
Ozzy-LXXVII <Ozzy.LXXVII@gmail.com>
2020-11-27 23:26:23 +0200

    added slider & products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
+    $('.intro-slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true
+    });
 
+    var mixer = mixitup('.products__items');
 });
